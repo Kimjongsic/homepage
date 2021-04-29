7879010f678bdd4c4ff7b5fa3ea88a5733cdd24c
Kimjongsic <whdtlr82@gmail.com>
2021-04-30 01:17:03 +0900

    mysqltest

--- mysqltest.php
+++ mysqltest.php
@@ -11,7 +11,7 @@
 echo "MySql 연결 테스트<br>";
 
     //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("mysql://b7ef317dd8b55f:21689411@us-cdbr-east-03.cleardb.com/heroku_0dc45199750e451?reconnect=true"));
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
 $cleardb_username = $cleardb_url["b7ef317dd8b55f"];
 $cleardb_password = $cleardb_url["21689411"];
