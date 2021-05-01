09fe822ef66917555a347333ee6fc978ad06d2d4
Kimjongsic <whdtlr82@gmail.com>
2021-05-02 04:25:05 +0900

    back wO.php

--- writeOk.php
+++ writeOk.php
@@ -11,26 +11,12 @@ $content = $_POST['content'];
 $date = date('Y-m-d');
 if($username  && $title && $content){
     $sql = mq("insert into mathboard(name,pw,title,content,date) values('".$username."','".$userpw."','".$title."','".$content."','".$date."')"); 
-    // echo "<script>
-    // alert('글쓰기 완료되었습니다.');
-    // location.href='mathboard.php';</script>";
+    echo "<script>
+    alert('글쓰기 완료되었습니다.');
+    location.href='mathboard.php';</script>";
 }else{
     echo "<script>
     alert('글쓰기에 실패했습니다.');
     history.back();</script>";
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php echo $_POST['pw'] ?>
-    <br>
-    <?php echo $userpw ?>
-</body>
-</html>
