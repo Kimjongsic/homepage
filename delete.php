5d8eec6c37073fe78cf284060c0031d37985e297
Kimjongsic <whdtlr82@gmail.com>
2021-05-02 00:34:08 +0900

    revise delete.php

--- delete.php
+++ delete.php
@@ -2,8 +2,10 @@
 	include $_SERVER['DOCUMENT_ROOT']."/mathboardSave.php";
 	
 	$bno = $_GET['num'];
+	$sql1 = mq("select * from mathboard where num='$bno';");
+	$board = $sql1->fetch_array();
 	if ($_SESSION['userName']==$board['name']) {
-	$sql = mq("delete from mathboard where num='$bno';");
+	$sql2 = mq("delete from mathboard where num='$bno';");
 ?>
 	<script type="text/javascript">alert("삭제되었습니다.");</script>
 	<meta http-equiv="refresh" content="0 url=/mathboard.php" />
