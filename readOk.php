c77a856f1580bee7b5c3fc99b89ea45b7d62fc47
Kimjongsic <whdtlr82@gmail.com>
2021-05-02 05:13:50 +0900

    revise rO

--- readOk.php
+++ readOk.php
@@ -5,7 +5,7 @@ $bno = $_GET['num'];
 $sql = mq("select * from mathboard where num='$bno';");
 $board = $sql->fetch_array();
 
-if ($board['lock_post'] == 1) {
+if ($board['lock_post'] == "1") {
     if ($_SESSION['userName']==$board['name']) {?>
         <script>location.href="read.php?num=<?php echo $board['num'];?>"</script>
     <?php }
