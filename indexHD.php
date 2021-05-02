24c643025168d60c6e33a4e6668ab24f0bee386f
Kimjongsic <whdtlr82@gmail.com>
2021-05-03 03:02:46 +0900

    create head.js

--- indexHD.php
+++ indexHD.php
@@ -38,27 +38,3 @@
             <a href="login.php" class="loginBtn">로그인</a>
         </div>
     </header>
-
-    <script>
-        function myBtn() {
-            document.getElementById("Dropdown").classList.toggle("show");
-        }
-        document.getElementById("Dropdown").addEventListener('click', function (event) {
-            event.stopPropagation();
-        });
-        window.onclick = function(event) {
-            if (!event.target.matches('.icon_bg') && !event.target.matches('.icon_img')) {
-              
-                var dropdowns = 
-                document.getElementsByClassName("hamberg");
-                  
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                    var openDropdown = dropdowns[i];
-                    if (openDropdown.classList.contains('show')) {
-                        openDropdown.classList.remove('show');
-                    }
-                }
-            }
-        }
-    </script>
