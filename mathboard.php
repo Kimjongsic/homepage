31e65a486ad4db111dd79a9e4708f66ce4d7a672
Kimjongsic <whdtlr82@gmail.com>
2021-05-03 00:05:17 +0900

    revise board

--- mathboard.php
+++ mathboard.php
@@ -25,7 +25,8 @@ include $_SERVER['DOCUMENT_ROOT']."/mathboardSave.php";
         </thead>
         <?php
             // board테이블에서 idx를 기준으로 내림차순해서 5개까지 표시
-          $sql = mq("select * from mathboard order by num desc limit 0,5"); 
+        //   $sql2 = mq("select * from mathboard order by num desc limit 0,5");
+        $sql = mq("select * from mathboard where num");
           while($mathboard = $sql->fetch_array())
           {
             //title변수에 DB에서 가져온 title을 선택
