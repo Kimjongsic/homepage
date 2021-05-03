a966e1222f289e48550af4cfdeb773f8c2623616
Kimjongsic <whdtlr82@gmail.com>
2021-05-03 14:45:21 +0900

    add board head

--- mathboard.php
+++ mathboard.php
@@ -9,14 +9,20 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="mathboard.css">
+    <link rel="stylesheet" href="head.css">
     <!-- <link rel="stylesheet" href="head.css"> -->
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/1c2538add9.js" crossorigin="anonymous"></script>
+    <script src="head.js"></script>
     <title>Document</title>
 </head>
 <body>
-
+<?php if(empty($_SESSION['userId'])) {
+    include "indexHD.php";
+} else {
+    include "homeHD.php";
+}?>
     <h1>수학 게시판</h1>
     <table class="list-table">
         <thead>
