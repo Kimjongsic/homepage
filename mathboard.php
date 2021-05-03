fd4d1d8d90cf59d1894103ef1aa3d7b36a013284
Kimjongsic <whdtlr82@gmail.com>
2021-05-03 14:27:07 +0900

    revise board

--- mathboard.php
+++ mathboard.php
@@ -31,13 +31,8 @@ session_start();
             // board테이블에서 idx를 기준으로 내림차순해서 5개까지 표시
           $sql2 = mq("select * from mathboard order by num desc limit 0,5");
           $mathboard = $sql->fetch_array();
-            //title변수에 DB에서 가져온 title을 선택
-            $title=$mathboard["title"]; 
-            if(strlen($title)>30)
-            { 
-              //title이 30을 넘어서면 ...표시
-              $title=str_replace($mathboard["title"],iconv_substr($mathboard["title"],0,30,"utf-8")."...",$mathboard["title"]);
-            }?>
+          echo $mathboard
+    ?>
         <tbody>
             <tr>
                 <td width="70"><?php echo $mathboard['num']; ?></td>
