63b6b5a67ffbc39e0fdca19d7c619d330efd3e13
Kimjongsic <whdtlr82@gmail.com>
2021-05-03 13:00:58 +0900

    delete board

--- mathboard.php
+++ mathboard.php
@@ -25,26 +25,6 @@ include $_SERVER['DOCUMENT_ROOT']."/mathboardSave.php";
             <th width="100">작성일</th>
             <th width="100">조회수</th>
         </thead>
-        <?php
-            // board테이블에서 idx를 기준으로 내림차순해서 5개까지 표시
-          $sql2 = mq("select * from mathboard order by num desc limit 0,5");
-          $mathboard = $sql->fetch_array();
-            //title변수에 DB에서 가져온 title을 선택
-            $title=$mathboard["title"]; 
-            if(strlen($title)>30)
-            { 
-              //title이 30을 넘어서면 ...표시
-              $title=str_replace($mathboard["title"],iconv_substr($mathboard["title"],0,30,"utf-8")."...",$mathboard["title"]);
-            }?>
-        <tbody>
-            <tr>
-                <td width="70"><?php echo $mathboard['num']; ?></td>
-                <td width="500"><a href="readOk.php?num=<?php echo $mathboard['num'];?>"><?php echo $title; ?></a></td>
-                <td width="120"><?php echo $mathboard['name']; ?></td>
-                <td width="100"><?php echo $mathboard['date']; ?></td>
-                <td width="100"><?php echo $mathboard['hit']; ?></td>
-            </tr>
-        </tbody>
     </table>
     <div id="write_btn">
         <a href="write.php"><button>글쓰기</button></a>
