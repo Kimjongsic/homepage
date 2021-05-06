a252d783b3e89cc23076e5d32dc6517b9649f09b
Kimjongsic <whdtlr82@gmail.com>
2021-05-06 11:30:53 +0900

    chage modify page

--- modifyOk.php
+++ modifyOk.php
@@ -5,7 +5,12 @@ $bno = $_GET['num'];
 $userpw = password_hash($_POST['pw'], PASSWORD_DEFAULT);
 $title = $_POST['title'];
 $content = $_POST['content'];
-$sql = mq("update mathboard set pw='".$userpw."',title='".$title."',content='".$content."' where num='".$bno."'"); ?>
+if(isset($_POST['lockpost'])){
+	$lo_post = '1';
+}else{
+	$lo_post = '0';
+}
+$sql = mq("update mathboard set pw='".$userpw."',title='".$title."',content='".$content."',lock_post='".$lo_post."' where num='".$bno."'"); ?>
 
 <script type="text/javascript">alert("수정되었습니다."); </script>
 <meta http-equiv="refresh" content="0 url=/read.php?idx=<?php echo $bno; ?>">
