29232e0d32fa296cb65fca78cd30f1f539c93fd9
Kimjongsic <whdtlr82@gmail.com>
2021-05-08 02:46:18 +0900

    return modifyok

--- modify_ok.php
+++ modify_ok.php
@@ -9,12 +9,8 @@ if($_POST['lockpost']=="1"){
 }else{
 	$lo_post = '0';
 }
-$sql = mq("update mathboard set title='".$title."',content='".$content."',lock_post='".$lo_post."' where num='".$bno."'");
-$sql2 = mq("select * from mathboard where num='".$bno."'");
-$board = $sql2->fetch_array();
-echo $board;
-?>
+$sql = mq("update mathboard set title='".$title."',content='".$content."',lock_post='".$lo_post."' where num='".$bno."'");?>
 
 
 <script type="text/javascript">alert("수정되었습니다."); </script>
-<!-- <meta http-equiv="refresh" content="0 url=/read.php?num=<?php echo $bno; ?>"> -->
+<meta http-equiv="refresh" content="0 url=/read.php?num=<?php echo $bno; ?>">
