da4ab6cd658dbb814799ef7e59d559a1cea1678e
Kimjongsic <whdtlr82@gmail.com>
2021-05-08 19:05:20 +0900

    create folder

--- mathboard/read.php
+++ mathboard/read.php
@@ -9,8 +9,8 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="read.css">
-	<link rel="stylesheet" href="head.css">
+    <link rel="stylesheet" href="/css/read.css">
+	<link rel="stylesheet" href="/css/head.css">
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/1c2538add9.js" crossorigin="anonymous"></script>
@@ -19,9 +19,9 @@ session_start();
 </head>
 <body>
 <?php if(empty($_SESSION['userId'])) {
-    include "indexHD.php";
+    include "/indexHD.php";
 }else {
-    include "homeHD.php";
+    include "/homeHD.php";
 }?>
 <?php
 		$bno = $_GET['num']; /* bno함수에 idx값을 받아와 넣음*/
