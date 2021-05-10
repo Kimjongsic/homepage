637899fdf135e9b11cd89e9b0d586d41321d231f
Kimjongsic <whdtlr82@gmail.com>
2021-05-10 23:50:49 +0900

    nobigdeal

--- mathboard/mathboard.php
+++ mathboard/mathboard.php
@@ -83,9 +83,9 @@ session_start();
               for($i=$block_start; $i<=$block_end; $i++){ 
                 //for문 반복문을 사용하여, 초기값을 블록의 시작번호를 조건으로 블록시작번호가 마지박블록보다 작거나 같을 때까지 $i를 반복시킨다
                 if($page == $i){ //만약 page가 $i와 같다면 
-                  echo "<strong>$i<strong>"; //현재 페이지에 해당하는 번호에 굵은 빨간색을 적용한다
+                  echo "<strong>$i</strong>"; //현재 페이지에 해당하는 번호에 굵은 빨간색을 적용한다
                 }else{
-                  echo "<a href='?page=$i'>[$i]</a>"; //아니라면 $i
+                  echo "<a href='?page=$i'>$i</a>"; //아니라면 $i
                 }
               }
             ?>
