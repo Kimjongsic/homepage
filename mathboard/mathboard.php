bf36528226e7d3bf310d5b4658e27c6fa83ebb57
Kimjongsic <whdtlr82@gmail.com>
2021-05-10 23:44:45 +0900

    no big deal

--- mathboard/mathboard.php
+++ mathboard/mathboard.php
@@ -77,7 +77,20 @@ session_start();
         </tbody>
         <?php }?>
     </table>
-    <?php include "../blockpgnum.php"; ?>
+    <div class="page_num_wrap">
+        <div id="page_num">
+            <?php
+              for($i=$block_start; $i<=$block_end; $i++){ 
+                //for문 반복문을 사용하여, 초기값을 블록의 시작번호를 조건으로 블록시작번호가 마지박블록보다 작거나 같을 때까지 $i를 반복시킨다
+                if($page == $i){ //만약 page가 $i와 같다면 
+                  echo "<strong>$i<strong>"; //현재 페이지에 해당하는 번호에 굵은 빨간색을 적용한다
+                }else{
+                  echo "<a href='?page=$i'>[$i]</a>"; //아니라면 $i
+                }
+              }
+            ?>
+        </div>
+    </div>
     <div class="btn_div">
         <div id="btn_wrap">
             <a class="write_btn" href="write.php">글쓰기</a>
