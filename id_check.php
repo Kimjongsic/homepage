d63f40a55e6b0ac55dcc646348d33f907b31438a
Kimjongsic <whdtlr82@gmail.com>
2021-05-10 20:30:47 +0900

    no big deal

--- id_check.php
+++ id_check.php
@@ -21,14 +21,14 @@ function mq($sql) {
 	$id_check = $id_check->fetch_array();
 	
 	if($id_check >= 1){
-		echo "<style type="text/css">
+		echo "<style type='text/css'>
                 .id_check {
                     color: rgb(234, 67, 53);
                     font-size: 12px;
                 }
             </style> 이미 사용중인 아이디입니다.";
 	} else {
-		echo "<style type="text/css">
+		echo "<style type='text/css'>
                 .id_check {
                     color: rgb(52, 168, 83);
                     font-size: 12px;
