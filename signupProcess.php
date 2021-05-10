8d062101f85a28055e536deebc53f4ab2298b01b
Kimjongsic <whdtlr82@gmail.com>
2021-05-11 01:47:38 +0900

    no bigdeal

--- signupProcess.php
+++ signupProcess.php
@@ -8,6 +8,12 @@ $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
 // Create connection
 $conn = new mysqli($hostname, $username, $password, $database);
+$conn->set_charset("utf8");
+
+function mq($sql) {
+    global $conn;
+    return $conn->query($sql);
+}
 
 $userid = $_POST['user_id'];
 $userpw = $_POST['user_pw'];
@@ -15,7 +21,7 @@ $userpwcf = $_POST['user_pw_confirm'];
 $hashedPassword = password_hash($_POST['user_pw'], PASSWORD_DEFAULT);
 echo $hashedPassword;
 
-    $id_check = "SELECT * FROM members WHERE id='$userid'";
+    $id_check = mq("select * from members where id='$userid'");
 	$id_check = $id_check->fetch_array();
 	if($id_check >= 1){
 		echo "<script>alert('아이디가 중복됩니다.'); history.back();</script>";
@@ -23,7 +29,7 @@ echo $hashedPassword;
         echo "<script>alert('비밀번호가 일치하지 않습니다.'); history.back();</script>";
     }
     else{
-        $sql = "INSERT INTO members(id, pw, name, birthday, gender) VALUES('{$_POST['user_id']}', '{$hashedPassword}', '{$_POST['user_name']}', '{$_POST['user_year']}', '{$_POST['user_gender']}')";
+        $sql = mq("insert into members(id, pw, name, birthday, gender) values('{$_POST['user_id']}', '{$hashedPassword}', '{$_POST['user_name']}', '{$_POST['user_year']}', '{$_POST['user_gender']}')");
         echo $sql;
         $result = mysqli_query($conn, $sql);
         if ($result === false) {
