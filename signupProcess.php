8d79b218c33cb36235662c6294ef98534321033a
Kimjongsic <whdtlr82@gmail.com>
2021-05-11 09:39:55 +0900

    confirm signup

--- signupProcess.php
+++ signupProcess.php
@@ -24,26 +24,22 @@ $userpwcf = $_POST['user_pw_confirm'];
 $hashedPassword = password_hash($_POST['user_pw'], PASSWORD_DEFAULT);
 echo $hashedPassword;
 
-    $id_check = mq("select * from members where id='$userid'");
-	$id_check = $id_check->fetch_array();
-	if($id_check >= 1){
-		echo "<script>alert('아이디가 중복됩니다.'); history.back();</script>";
-	}elseif($userpw !== $userpwcf){
-        echo "<script>alert('비밀번호가 일치하지 않습니다.'); history.back();</script>";
+$id_check = mq("select * from members where id='$userid';");
+$id_check = $id_check->fetch_array();
+if($id_check >= 1){
+	echo "<script>alert('아이디가 중복됩니다.'); history.back();</script>";
+}elseif($userpw !== $userpwcf){
+    echo "<script>alert('비밀번호가 일치하지 않습니다.'); history.back();</script>";
+}
+else{
+    $sql = mq("insert into members(id,pw,name,birthday,gender) values('".$userid."','".$hashedPassword."','".$username."','".$useryear."','".$usersex."')");
+    echo $sql;
+    $result = mysqli_query($conn, $sql);
+    echo $result;
+    if ($result === false) {
+        echo "저장에 문제가 생겼습니다. 관리자에게 문의해주세요.";
+        echo mysqli_error($conn);
+    } else {
+        echo "<script>alert('회원가입이 완료되었습니다.'); location.href='login.php';</script>";
     }
-    else{
-        $sql = mq("insert into members(id, pw, name, birthday, gender) values('".$userid."', '".$hashedPassword."', '".$username."', '".$useryear."', '".$usersex."')");
-        echo $sql;
-        $result = mysqli_query($conn, $sql);
-        if ($result === false) {
-            echo "저장에 문제가 생겼습니다. 관리자에게 문의해주세요.";
-            echo mysqli_error($conn);
-        } else {
-        ?>
-            <script>
-                alert("회원가입이 완료되었습니다");
-                location.href = "login.php";
-            </script>
-        <?php
-        };
-    }?>
+}?>
